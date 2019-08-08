9fdd41b96ecce38430bfd1bc89cd14ce8ed0430d
kpcruz <kelvinmembership@gmail.com>
2019-08-07 17:58:33 -0700

    added folders and Navbar via reactstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import NavBar from "./Navbar";
+import NavBar from "./components/NavBar";
 
 function App() {
 	return (
