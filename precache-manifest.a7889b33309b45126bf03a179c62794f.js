f1291af26f8370bc47c88380850627227515acb8
kpcruz <kelvinmembership@gmail.com>
2019-08-09 17:19:11 -0700

    Updates

---
+++ precache-manifest.a7889b33309b45126bf03a179c62794f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388e25da50731bc7cfbcf5df0a386bec",
+    "url": "/bday2019/index.html"
+  },
+  {
+    "revision": "6bd92b7de627d5df8d12",
+    "url": "/bday2019/static/css/main.22cd0e5a.chunk.css"
+  },
+  {
+    "revision": "3fff086d9e1c19c01a84",
+    "url": "/bday2019/static/js/2.27200e24.chunk.js"
+  },
+  {
+    "revision": "6bd92b7de627d5df8d12",
+    "url": "/bday2019/static/js/main.b4aa75dd.chunk.js"
+  },
+  {
+    "revision": "a6eb123363d23b3074dd",
+    "url": "/bday2019/static/js/runtime~main.94045ab4.js"
+  },
+  {
+    "revision": "35f8f4dfd05c421eca33880d7912c273",
+    "url": "/bday2019/static/media/contact.35f8f4df.gif"
+  },
+  {
+    "revision": "4304a30a2c111daf43b35f50463215d7",
+    "url": "/bday2019/static/media/kp.4304a30a.png"
+  }
+]);
